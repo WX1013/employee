1f76e54b117d92aed9dcb8fc315479ededf53ed3
wx <iamwx@foxmail.com>
2019-04-14 14:50:26 +0800

    update

--- src/main/java/com/emp/service/impl/EmpServiceImpl.java
+++ src/main/java/com/emp/service/impl/EmpServiceImpl.java
@@ -45,12 +45,12 @@ public class EmpServiceImpl implements EmpService {
     @Override
     public Integer save(EmpEntity entity) {
         int count;
-        if(entity.getId() == null){ // 如果id为空，即为增加
+        if (entity.getId() == null) { // 如果id为空，即为增加
             entity.setDelFlg(0);
             entity.setAddTime(new Date());
-            entity.setUpdateTime(new Date() );
+            entity.setUpdateTime(new Date());
             count = empEntityMapper.insertSelective(entity);
-        }else{ // 如果id不为空，即为修改
+        } else { // 如果id不为空，即为修改
             entity.setUpdateTime(new Date());
             count = empEntityMapper.updateByPrimaryKey(entity);
         }
@@ -69,19 +69,19 @@ public class EmpServiceImpl implements EmpService {
         EmpEntity entity = empEntityMapper.selectByPrimaryKey(id);
         DeptEntity dept = deptEntityMapper.selectByPrimaryKey(entity.getDeptId());
         entity.setDeptName(dept == null ? "" : dept.getName());
-        entity.setAddTimeStr(CommonUtil.date2String(entity.getAddTime(),"yyyy-MM-dd"));
+        entity.setAddTimeStr(CommonUtil.date2String(entity.getAddTime(), "yyyy-MM-dd"));
         return entity;
     }
 
     @Override
     public PageResult findPage(EmpEntity emp, int pageNum, int pageSize) {
         // 判断缓存中是否有数据
-        if(redisTemplate.opsForList().size("emps") > 0) {
+        if (redisTemplate.opsForList().size("emps") > 0) {
             // 如果缓存中有数据，则从缓存中查询数据
             List<EmpEntity> range = redisTemplate.opsForList().range("emps", 0, -1);
             List<EmpEntity> pageResult = new ArrayList<>();
             int upCount = (pageNum - 1) * pageSize + pageSize;
-            if(upCount >= range.size()){
+            if (upCount >= range.size()) {
                 upCount = range.size();
             }
             for (int i = (pageNum - 1) * pageSize; i < upCount; i++) {
@@ -90,22 +90,22 @@ public class EmpServiceImpl implements EmpService {
             int total = range.size();
             int pages = total / pageSize + 1;
             return new PageResult(pageNum, pages, total, pageResult);
-        }else{
+        } else {
             // 如果缓存中没有，则从数据库中查并且存数据到缓存中
             List<EmpEntity> emps = empEntityMapper.selectByExample(null);
             for (EmpEntity empEntity : emps) {
                 redisTemplate.opsForList().rightPush("emps", empEntity);
             }
             PageHelper.startPage(pageNum, pageSize);
-            EmpEntityExample example=new EmpEntityExample();
+            EmpEntityExample example = new EmpEntityExample();
             EmpEntityExample.Criteria criteria = example.createCriteria();
-            if(emp!=null){
-                if(emp.getName()!=null && emp.getName().length()>0){
-                    criteria.andNameLike("%"+emp.getName()+"%");
+            if (emp != null) {
+                if (emp.getName() != null && emp.getName().length() > 0) {
+                    criteria.andNameLike("%" + emp.getName() + "%");
                 }
             }
-            Page<EmpEntity> page= (Page<EmpEntity>)empEntityMapper.selectByExample(example);
-            return new PageResult(pageNum,page.getPages(),page.getTotal(), page.getResult());
+            Page<EmpEntity> page = (Page<EmpEntity>) empEntityMapper.selectByExample(example);
+            return new PageResult(pageNum, page.getPages(), page.getTotal(), page.getResult());
         }
     }
 
